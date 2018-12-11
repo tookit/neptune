390b200765cafc0093c7a23f9caf2dddf8148a70
tookit <wangqiangshen@gmail.com>
2018-12-11 18:09:07 +0800

    add query  builder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->comment('Username');
             $table->string('email')->unique();
+            $table->string('mobile');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
